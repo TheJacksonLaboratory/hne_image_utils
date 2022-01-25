e78c6e360b5586a8ed53b8755db71aaf11cf4220
Brian White <brian.white@jax.org>
2022-01-25 22:57:25 +0000

    Add function to parse Hover-Net cell type info from type_info.json.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # hne_image_utils
 
+A python package providing utility functions for downstream analysis
+and visualization of H&E images. This includes:
+- hovernet_utils.py: parsing Hover-Net (cell segmentation and cell type calling) output
+
--- hovernet_utils.py
+++ hovernet_utils.py
@@ -30,7 +30,10 @@ def summarize_hovernet_cells(json_path):
     Returns:
         a tuple containing
 
-        - df (DataFrame): 
+        - df (DataFrame): a DataFrame with columns cell_type (number to
+          be cross referenced with HoverNet type_info.json file), cell_type_area
+          (the total area of all cells of cell_type), and cell_type_counts
+          (the total number of cells of cell_type).
         - mag (float): the magnification of the image Hover-Net was run on
     """
     count_dict = {}
@@ -64,3 +67,78 @@ def summarize_hovernet_cells(json_path):
     counts = [count_dict[k] for k in keys]
     df = pd.DataFrame({"cell_type": keys, "cell_type_area": areas, "cell_type_counts": counts})
     return df, mag_info
+
+def extract_cell_info_from_hovernet_output(json_path):
+    """Extract cell info (e.g., centroid location and cell type) from Hover-Net JSON output.
+
+    Parameter:
+        json_path (str): path to json file output by Hover-Net
+
+    Returns:
+        a tuple containing
+
+        - df (DataFrame): a DataFrame with columns cell_type (number to
+          be cross referenced with HoverNet type_info.json file), cell_type_area
+          (the total area of all cells of cell_type), and cell_type_counts
+          (the total number of cells of cell_type).
+        - mag (float): the magnification of the image Hover-Net was run on
+    """
+    json_file = open(json_path)
+    data = json.load(json_file)
+
+    mag_info = data['mag']
+    nuc_info = data['nuc']
+    n_cells = len(nuc_info)
+
+    # lists to store centroid x and y coordinates, cell type, cell area, and cell id
+    centroid_xs = np.empty((n_cells,), dtype=float)
+    centroid_ys = np.empty((n_cells,), dtype=float)
+    cell_types = np.empty((n_cells,), dtype=np.uint8)
+    cell_areas = np.empty((n_cells,), dtype=float)
+    ids = np.empty((n_cells,), dtype=np.uint8)
+
+    keys = list(nuc_info.keys())
+    for i in range(0, n_cells):
+        inst_info = nuc_info[keys[i]]
+        inst_centroid = inst_info['centroid']
+        inst_contour = inst_info['contour']
+        inst_type = inst_info['type']
+        x=np.asarray(inst_contour)[:,0]
+        y=np.asarray(inst_contour)[:,1]
+        area = polygon_area(x, y)
+        centroid_xs[i] = inst_centroid[0]
+        centroid_ys[i] = inst_centroid[1]
+        cell_types[i] = inst_type
+        cell_areas[i] = area
+        ids[i] = keys[i]
+
+    df = pd.DataFrame({"ids": ids, "centroid_x": centroid_xs, "centroid_y": centroid_ys, "cell_type": cell_types, "cell_type_area": cell_areas})
+    return df, mag_info
+
+def extract_hovernet_cell_info(json_path):
+    """Extract cell type info from Hover-Net type_info.json file
+
+    Parameter:
+        json_path (str): path to type_info.json file input to Hover-Net
+
+    Returns:
+        DataFrame with columns:
+        - id (object): the cell type id (e.g., as included in the Hover-Net JSON output)
+        - label (object): the label of the cell type (e.g., neopla, etc)
+        - r_color: the red channel of the RGB color specification used by Hover-Net for this cell type
+        - g_color: the green channel of the RGB color specification used by Hover-Net for this cell type
+        - b_color: the blue channel of the RGB color specification used by Hover-Net for this cell type
+    """
+
+    json_file = open(json_path)
+    cell_dict = json.load(json_file)
+    
+    cell_tbl = pd.DataFrame.from_dict(cell_dict, orient="index", columns = ['label', 'rgb'])
+    cell_tbl['r_color'] = cell_tbl.rgb.apply(lambda x: x[0])
+    cell_tbl['g_color'] = cell_tbl.rgb.apply(lambda x: x[1])
+    cell_tbl['b_color'] = cell_tbl.rgb.apply(lambda x: x[1])
+    cell_tbl = cell_tbl.drop(labels=['rgb'], axis=1)
+    cell_tbl = cell_tbl.reset_index().rename(columns={'index':'id'})
+
+    return cell_tbl
+    
