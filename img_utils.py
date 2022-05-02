cd307f048ff9b13fc69846716c8213975ca51952
Brian White <brian.white@jax.org>
2022-05-02 11:05:17 -0700

    Add function to extract tile

--- img_utils.py
+++ img_utils.py
@@ -2,6 +2,7 @@ import json
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
+import openslide
 
 def count_non_zero_pixels(image_file):
     """Calculate the number of non-zero pixels in an image.
@@ -16,3 +17,17 @@ def count_non_zero_pixels(image_file):
     nz_pixels = np.count_nonzero(im)
     return nz_pixels
 
+def extract_tile(image_file, minx, miny, width, height):
+    """Extract a tile from an image. 
+
+    Parameter:
+        image_file (str): path to image file (compatible with openslide.open_slide)
+        minx, miny: top left pixel coordinate in the level 0 reference frame
+        width, height: width and height in pixels in level 0 reference frame
+
+    Returns:
+        image: tile 
+    """
+    slide = openslide.open_slide(image_file)
+    im = slide.read_region((minx, miny), 0, (width, height))
+    return im
